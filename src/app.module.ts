c202a0ee231a32ea90f6de7c88f592a3c573a5d3
Andre Grossi <iamandregrossi@gmail.com>
2020-11-11 20:16:34 -0300

    Install graphql tools and add module into app.module as well as app resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,37 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
 
 import { AppController } from './app.controller'
 import config from './configs/config'
+import { GraphqlConfig } from './configs/config.interface'
+import { AppResolver } from './resolvers/app.resolver'
+// import { UserModule } from './resolvers/user/user.module'
 import { AppService } from './services/app.service'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [config] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const graphqlConfig = configService.get<GraphqlConfig>('graphql')
+        return {
+          buildSchemaOptions: {
+            numberScalarMode: 'integer'
+          },
+          sortSchema: graphqlConfig.sortSchema,
+          autoSchemaFile:
+            graphqlConfig.schemaDestination || './src/schema.graphql',
+          debug: graphqlConfig.debug,
+          playground: graphqlConfig.playgroundEnabled,
+          context: ({ req }) => ({ req })
+        }
+      },
+      inject: [ConfigService]
+    })
+    // UserModule
+  ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, AppResolver]
 })
 export class AppModule {}
