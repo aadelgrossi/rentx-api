47b3cb582304bffae7789cb1337d93f320343d7c
Andre Grossi <iamandregrossi@gmail.com>
2020-11-11 14:10:09 -0300

    Initial commit + eslint/prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'standard',
+    'prettier/standard',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es2021: true
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+    "import/order": [
+      "error",
+      {
+        "groups": ["builtin", "external", "internal"],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
+  },
+};
