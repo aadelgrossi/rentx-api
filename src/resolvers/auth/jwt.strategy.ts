986772ec3a5eac72ec4dc2b4637aea8cded106a4
Andre Grossi <iamandregrossi@gmail.com>
2020-11-11 23:02:53 -0300

    Add validate function on JWT strategy to check whether user is authorized

--- src/resolvers/auth/jwt.strategy.ts
+++ src/resolvers/auth/jwt.strategy.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy, ExtractJwt } from 'passport-jwt'
+import { User } from 'src/models/user.model'
 import { AuthService } from 'src/services/auth.service'
+
+import { JwtDto } from './dto/jwt.dto'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,5 +17,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get('JWT_SECRET')
     })
+  }
+
+  async validate(payload: JwtDto): Promise<User> {
+    const user = await this.authService.validateUser(payload.userId)
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return user
   }
 }
