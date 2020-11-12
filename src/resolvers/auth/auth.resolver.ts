cc268fe0c0b0b25d5dc5ce394b7ebb6905fee389
Andre Grossi <iamandregrossi@gmail.com>
2020-11-11 23:02:01 -0300

    Update auth resolver with signIn and signUp functions

--- src/resolvers/auth/auth.resolver.ts
+++ src/resolvers/auth/auth.resolver.ts
@@ -1,4 +1,31 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Auth } from 'src/models/auth.model'
+import { AuthService } from 'src/services/auth.service'
 
-@Resolver()
-export class AuthResolver {}
+import { SigninInput } from './dto/signin.input'
+import { SignupInput } from './dto/signup.input'
+
+@Resolver(() => Auth)
+export class AuthResolver {
+  constructor(private readonly auth: AuthService) {}
+
+  @Mutation(() => Auth)
+  async signup(@Args('data') data: SignupInput) {
+    data.email = data.email.toLowerCase()
+    const { accessToken, refreshToken } = await this.auth.createUser(data)
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
+  @Mutation(() => Auth)
+  async signin(@Args('data') { email, password }: SigninInput) {
+    const { accessToken, refreshToken } = await this.auth.login({
+      email: email.toLowerCase(),
+      password
+    })
+
+    return { accessToken, refreshToken }
+  }
+}
