9e43604e2dc99942abda75efa4a5c4e7bd50a980
Andre Grossi <iamandregrossi@gmail.com>
2020-12-27 15:48:58 -0300

    Create DTOs for create Car mutation

---
+++ src/resolvers/car/dto/create_car.input.ts
@@ -0,0 +1,26 @@
+import { InputType, Field } from '@nestjs/graphql'
+
+import { Transmission, FuelType } from '../../../models/car.model'
+import { CreateManufacturerInput } from './create_manufacturer.input'
+import { CreatePhotoInput } from './create_photo.input'
+
+@InputType()
+export class CreateCarInput {
+  @Field()
+  name: string
+
+  @Field()
+  dailyRate: number
+
+  @Field()
+  fuelType: FuelType
+
+  @Field()
+  transmission: Transmission
+
+  @Field()
+  manufacturer: CreateManufacturerInput
+
+  @Field()
+  photo?: CreatePhotoInput
+}
