1e99491beb5ab77202277cd72d6c18f6555ff473
Andre Grossi <iamandregrossi@gmail.com>
2020-12-28 01:49:05 -0300

    Update resolver and add resolved fields for association retrieval; add get user rentals query

--- src/modules/rental/rental.resolver.ts
+++ src/modules/rental/rental.resolver.ts
@@ -1,4 +1,12 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver
+} from '@nestjs/graphql'
+import { PrismaService } from 'src/services'
 
 import { CreateRentalInput } from './dto/create_rental.input'
 import { Rental } from './rental.model'
@@ -6,10 +14,40 @@ import { RentalService } from './rental.service'
 
 @Resolver(() => Rental)
 export class RentalResolver {
-  constructor(private readonly rentalService: RentalService) {}
+  constructor(
+    private readonly rentalService: RentalService,
+    private readonly prisma: PrismaService
+  ) {}
+
+  @Query(() => [Rental])
+  async getRentalsForUser(@Args('id') id: string): Promise<Rental[]> {
+    return this.rentalService.forUser(id)
+  }
 
   @Mutation(() => Rental)
   async createRental(@Args('data') data: CreateRentalInput): Promise<Rental> {
     return this.rentalService.create(data)
+  }
+
+  @ResolveField('user')
+  async user(@Parent() rental: Rental) {
+    return await this.prisma.rental
+      .findOne({
+        where: {
+          id: rental.id
+        }
+      })
+      .user()
+  }
+
+  @ResolveField('car')
+  async car(@Parent() rental: Rental) {
+    return await this.prisma.rental
+      .findOne({
+        where: {
+          id: rental.id
+        }
+      })
+      .car()
   }
 }
