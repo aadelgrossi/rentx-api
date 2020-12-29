3fd93c725208cbc7f186685fa78cf01f14339a27
Andre Grossi <iamandregrossi@gmail.com>
2020-12-29 14:45:30 -0300

    Create and register CarSpecification resolver to resolve nested field

--- src/modules/car/car.module.ts
+++ src/modules/car/car.module.ts
@@ -3,8 +3,9 @@ import { PrismaService } from 'src/services/prisma.service'
 
 import { CarResolver } from './car.resolver'
 import { CarService } from './car.service'
+import { CarSpecificationResolver } from './car_specification.resolver'
 
 @Module({
-  providers: [PrismaService, CarResolver, CarService]
+  providers: [PrismaService, CarResolver, CarSpecificationResolver, CarService]
 })
 export class CarModule {}
---
+++ src/modules/car/car_specification.resolver.ts
@@ -0,0 +1,18 @@
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql'
+import { PrismaService } from 'src/services'
+
+import { CarSpecification } from './models'
+
+@Resolver(() => CarSpecification)
+export class CarSpecificationResolver {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @ResolveField('specification')
+  async specification(@Parent() { id }: CarSpecification) {
+    return await this.prisma.carSpecification
+      .findOne({
+        where: { id }
+      })
+      .specification()
+  }
+}
