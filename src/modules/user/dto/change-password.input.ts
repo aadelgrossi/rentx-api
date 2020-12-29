20a20c65da1c092c559f71d1a47859020d43ed70
Andre Grossi <iamandregrossi@gmail.com>
2020-12-29 00:48:54 -0300

    Cleanup decorators for user model and dto

--- src/modules/user/dto/change-password.input.ts
+++ src/modules/user/dto/change-password.input.ts
@@ -1,14 +1,12 @@
-import { InputType, Field } from '@nestjs/graphql'
+import { InputType } from '@nestjs/graphql'
 import { IsNotEmpty, MinLength } from 'class-validator'
 
 @InputType()
 export class ChangePasswordInput {
-  @Field()
   @IsNotEmpty()
   @MinLength(8)
   oldPassword: string
 
-  @Field()
   @IsNotEmpty()
   @MinLength(8)
   newPassword: string
--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -4,21 +4,18 @@ import { BaseModel, Photo } from 'src/models'
 import { Rental } from '../rental/rental.model'
 @ObjectType()
 export class User extends BaseModel {
-  @Field()
   email: string
 
-  @Field()
   name: string
 
   @Field({ defaultValue: false })
   admin: boolean
 
-  @Field(() => Photo)
   avatar?: Photo
 
   @HideField()
   password: string
 
-  @Field()
-  rentals?: Rental
+  @Field(() => [Rental], { nullable: true })
+  rentals?: [Rental]
 }
