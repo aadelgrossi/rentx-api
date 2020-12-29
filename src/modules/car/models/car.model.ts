55435697ab5116c8b68cbc46b56b1b9ce0274f23
Andre Grossi <iamandregrossi@gmail.com>
2020-12-29 00:31:12 -0300

    Cleanup Car model and add relationship to CarSpecification

--- src/modules/car/models/car.model.ts
+++ src/modules/car/models/car.model.ts
@@ -1,49 +1,19 @@
-import { Field, ObjectType, registerEnumType } from '@nestjs/graphql'
+import { Field, ObjectType } from '@nestjs/graphql'
 import { BaseModel, Photo } from 'src/models'
 
 import { CarManufacturer, CarSpecification } from './index'
-
-export enum FuelType {
-  gas = 'gas',
-  electric = 'electric',
-  hybrid = 'hybrid'
-}
-
-export enum Transmission {
-  auto = 'auto',
-  manual = 'manual'
-}
-
-registerEnumType(FuelType, {
-  name: 'FuelType',
-  description: 'Allowed fuel types for car'
-})
-
-registerEnumType(Transmission, {
-  name: 'Transmission',
-  description: 'Allowed transmission modes for car'
-})
-
 @ObjectType()
 export class Car extends BaseModel {
-  @Field()
-  name: string
+  model: string
 
-  @Field(() => CarManufacturer)
   manufacturer?: CarManufacturer
 
-  @Field()
+  fullName?: string
+
   dailyRate: number
 
-  @Field(() => Photo)
   photo?: Photo
 
-  @Field(() => FuelType)
-  fuelType: string
-
-  @Field(() => Transmission)
-  transmission: string
-
   @Field(() => [CarSpecification], { nullable: true })
-  specifications?: [CarSpecification] | null
+  specifications?: [CarSpecification]
 }
